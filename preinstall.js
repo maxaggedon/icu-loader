02dbe3f7f83bab687d9535193838dfd608ecde6e
Masoud Ghorbani <msud.ghorbani@gmail.com>
2017-07-30 19:50:29 +0430

    inform users that try to update previous version to 3.0.0

---
+++ preinstall.js
@@ -0,0 +1,5 @@
+'use strict'
+
+const chalk = require('chalk')
+
+console.log(chalk.bold.red('⚠ Warning:'), `Since this version that you installed the syntax of package has been changed to Promise way, to figure out check the documentation of library. (Backwards incompatible)`)
